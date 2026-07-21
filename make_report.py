39351203e1a04f6564bdb6f57252a8a7e5e3075d
NathanBachmann <nathan.bachmann@agrf.org.au>
2026-07-21 16:20:29 +1000

    More foundations for the template

--- make_report.py
+++ make_report.py
@@ -4,6 +4,7 @@ import jinja2
 from jinja2 import Environment, FileSystemLoader, Template
 from pathlib import Path
 import os
+import shutil
 
 #functions
 def get_script_dir():
@@ -13,8 +14,11 @@ def get_script_dir():
 #Set variables
 script_dir = get_script_dir()
 
+# Read parameters file
+with open("parameters.txt") as f:
+    parameters = f.read()
 
-# Load the .tsv file into a DataFrame
+# Load the stats.tsv file into a DataFrame
 file_path = "results/stats.tsv"  # Replace with the path to your .tsv file
 df = pd.read_csv(file_path, sep='\t')
 
@@ -28,10 +32,12 @@ template = env.get_template("main_report.html")
 # Render the template with the DataFrame data
 html_report = template.render(
     columns=df.columns.tolist(),
-    rows=df.values.tolist()
+    rows=df.values.tolist(),
+    parameters=parameters
 )
 
 # Save the rendered HTML to a file
 output_path = Path("report.html")
 output_path.write_text(html_report)
 
+shutil.copy(os.path.join(script_dir, "templates", "understanding_the_analysis.html"), ".")
